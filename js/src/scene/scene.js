56438048c940abaecd58b1e8af82bf6a7f1d61e0
Pirororo <sw.cont.211@gmail.com>
2020-12-29 16:02:10 +0900

    ok2

---
+++ js/src/scene/scene.js
@@ -0,0 +1,30 @@
+// import * as THREE from '../../libs/three.module.js';
+import Ring_colorLine from '../objects/Ring_9th_colorCirc.js';
+
+export class Scene extends THREE.Scene {
+
+    constructor(){
+
+        super();
+        this.scene = 0;
+
+        //カメラ
+        this._persCamera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 10, 500);
+        this.camera = this._persCamera; //初期値
+        this.camera.camPos = new THREE.Vector3(0, 0, -355);//9th
+        this.camera.position.set(this.camera.camPos.x,this.camera.camPos.y,this.camera.camPos.z);
+
+
+        //リング、色付き線
+        this._ring_colorLine = new Ring_colorLine();
+        this.add(this._ring_colorLine);
+
+    }
+
+    update(){
+
+        this.camera.lookAt(new THREE.Vector3(0, 0, 0));//必須！！
+        this._ring_colorLine.update();
+
+    }
+}
